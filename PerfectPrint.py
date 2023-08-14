aec26f94358ab4c7fa81653e4f46fc6756c60038
José Miguel <101609455+Zezenta@users.noreply.github.com>
2023-08-14 01:32:15 -0500

    Bitcoin Whitepaper as test document

--- PerfectPrint.py
+++ PerfectPrint.py
@@ -2,15 +2,18 @@ import PyPDF2 #the library that supports PDF manipulation
 import re #imports the regex that we are going to use
 from docx import Document #the library that supports docx manipulation
 
-file_name = input("Please enter the name of the file that you want to alter. EXAMPLE: document.pdf ")
-pattern = r'[\w\_\-]+\.(\w+)' #This regex indicates any kind of alphanumerical number, and "-" or "_" characters, one time or more. Then, a single period. And finally, captures any alphanumerical text (hence, the extension)
+file_raw_name = input("Please enter the name of the file that you want to alter. EXAMPLE: document.pdf ")
+pattern = r'([\w\_\-]+)\.(\w+)' #This regex indicates any kind of alphanumerical number, and "-" or "_" characters, one time or more. Then, a single period. And finally, captures any alphanumerical text (hence, the extension)
 
-file_type = re.match(pattern, file_name)
+file_groups = re.match(pattern, file_raw_name)
+file_name = file_groups.group(1)
+file_type = file_groups.group(2)
+
 
 current_file_pages = []
 
-if file_type.group(1) == "pdf":
-    with open(file_name, 'rb') as pdf_file:
+if file_type == "pdf":
+    with open(file_raw_name, 'rb') as pdf_file:
         pdf_reader = PyPDF2.PdfReader(pdf_file)
         for page_num in range(len(pdf_reader.pages)):
             page = pdf_reader.pages[page_num]
@@ -18,16 +21,7 @@ if file_type.group(1) == "pdf":
 
 reversed_pages = current_file_pages[::-1]
 
-output_file_name = file_name + "reversed"
+output_file_name = file_name + "_REVERSED" + "." + file_type
 
-def create_pdf(output_filename, pages):
-    pdf_writer = PyPDF2.PdfWriter()
-
-    for page_content in pages:
-        pdf_page = PyPDF2.PageObject.create_text_page(page_content)
-        pdf_writer.add_page(pdf_page)
-
-    with open(output_filename, 'wb') as output_file:
-        pdf_writer.write(output_file)
-
-create_pdf(output_file_name, reversed_pages)
+print(output_file_name)
+print(current_file_pages)
