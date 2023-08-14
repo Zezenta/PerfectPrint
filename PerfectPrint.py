4511130732dc6e4e6b42fb87b7fa30abbc7046f6
José Miguel <101609455+Zezenta@users.noreply.github.com>
2023-08-14 00:03:12 -0500

    Test files

--- PerfectPrint.py
+++ PerfectPrint.py
@@ -1,1 +1,7 @@
-print("hello world")
+import PyPDF2
+from docx import Document
+
+
+file_name = input("Please enter the name of the file that you want to alter. EXAMPLE: document.pdf")
+
+print("hello world", file_name)
